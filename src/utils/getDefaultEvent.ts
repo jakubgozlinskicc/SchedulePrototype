3bb5c3e0ba3d1f6988f1890c01ee10f0832a4b84
jakubgozlinskicc <jakubgozlinskicc@gmail.com>
2025-12-09 11:38:57 +0100

    getDefaultEvent added to utils

---
+++ src/utils/getDefaultEvent.ts
@@ -0,0 +1,10 @@
+import type { Event } from "../db/scheduleDb";
+
+export const getDefaultEvent = (): Event => ({
+  id: undefined,
+  title: "",
+  description: "",
+  start: new Date(),
+  end: new Date(),
+  color: "#0000FF",
+});
