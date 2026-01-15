1e20744f7203fa0e9249968cef8a54008fcf99d6
jakubgozlinskicc <jakubgozlinskicc@gmail.com>
2026-01-15 08:51:43 +0100

    all colors switch to variables final

--- src/pages/schedulePage/components/customToolbar/components/WeekStrip.tsx
+++ src/pages/schedulePage/components/customToolbar/components/WeekStrip.tsx
@@ -2,6 +2,7 @@ import { format, startOfWeek, addDays, isSameDay } from "date-fns";
 import type { NavigateAction } from "react-big-calendar";
 import { useTranslationContext } from "../../../../../locales/useTranslationContext";
 import { locales } from "../../../../../utils/calendarLocalizer/calendarLocalizer";
+import { Button } from "../../../../../components/Button/Button";
 
 type OnView = (view: "month" | "week" | "day") => void;
 
@@ -20,18 +21,18 @@ export const WeekStrip = ({ date, onNavigate, onView }: WeekStripProps) => {
   return (
     <div className="week-strip">
       {days.map((day) => (
-        <button
+        <Button
           key={day.toISOString()}
-          className={
-            "week-strip-day" + (isSameDay(day, date) ? " active-day" : "")
-          }
+          variant="secondary"
+          isActive={isSameDay(day, date)}
+          className="week-strip-day"
           onClick={() => {
             onView("day");
             onNavigate("DATE", day);
           }}
         >
           {format(day, "EEE dd", { locale })}
-        </button>
+        </Button>
       ))}
     </div>
   );
